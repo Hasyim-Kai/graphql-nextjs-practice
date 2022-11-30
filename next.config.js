4f57b80a3814eaf00d827ead54f85792ea3dbd55
ichihatsu1 <ichihatsu1@gmail.com>
2022-11-30 08:48:47 +0700

    Refactor IMG Config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.staticflickr.com',
+        // port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
