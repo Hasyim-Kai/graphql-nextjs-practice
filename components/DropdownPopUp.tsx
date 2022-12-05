6be9af35f77cf8cad0f1139e21d53feb64e0d502
ichihatsu1 <ichihatsu1@gmail.com>
2022-12-05 11:51:23 +0700

    Refactor Dropdown Click Outside

--- components/DropdownPopUp.tsx
+++ components/DropdownPopUp.tsx
@@ -4,10 +4,11 @@ import { useClickOutside } from '../hooks/useClickOutside';
 import s from '../styles/dropdown.module.scss'
 
 export default function DropdownPopUp() {
-    const ref = useRef<HTMLElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
     const [isOpen, setIsOpen] = useState(false);
     const handleChange = () => { setIsOpen((state) => !state) }
-    useClickOutside(ref, () => { setIsOpen(false) });
+    const closePopUp = () => { setIsOpen(false) }
+    useClickOutside(isOpen, ref, closePopUp);
 
     return <section className={s.wrapper} ref={ref}>
         <button onClick={handleChange} className={s.btn}>V</button>
--- hooks/useClickOutside.tsx
+++ hooks/useClickOutside.tsx
@@ -1,23 +1,21 @@
-import { useEffect, RefObject } from 'react';
+import { useEffect, RefObject, useCallback } from 'react';
 
 type Event = MouseEvent | TouchEvent;
 
-export const useClickOutside = <T extends HTMLElement = HTMLElement>(ref: RefObject<T>, handler: (event: Event) => void,) => {
-    useEffect(() => {
-        function handleClickOutside(event: Event) {
-            const el = ref?.current;
-            if (!el || el.contains((event?.target as Node) || null)) {
-                return;
-            }
-            handler(event); // Call the handler only if the click is outside of the element passed.
+export const useClickOutside = <T extends HTMLElement = HTMLElement>(isShow: boolean, ref: RefObject<T>, handler: Function) => {
+    const handleClickOutside = useCallback((event: Event) => {
+        const el = ref?.current;
+        // if (!el || el.contains((event?.target as Node) || null) || event.target === btnRef.current) {
+        if (!el || el.contains((event?.target as Node) || null)) {
+            return;
         }
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        document.addEventListener('touchstart', handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-            document.removeEventListener('touchstart', handleClickOutside);
-        };
-    }, [ref]);
+        handler(); // Call the handler only if the click is outside of the element passed.
+        document.removeEventListener("mousedown", handleClickOutside, true);
+        document.removeEventListener('touchstart', handleClickOutside, false);
+    }, [ref, isShow])
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside, true);
+        document.addEventListener('touchstart', handleClickOutside, false);
+    }, [isShow]);
 };
