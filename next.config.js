f1442a729a247c837296186cdc7f9ca6a4f3ac27
ichihatsu1 <ichihatsu1@gmail.com>
2022-12-05 11:50:34 +0700

    Refactor Next config js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, 'styles')],
-  // },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '*.staticflickr.com',
-        // port: '',
-        // pathname: '/account123/**',
       },
     ],
   },
